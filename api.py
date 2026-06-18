cea2ffa07ef9832d7b0a7119e47c3915a491305b
robkarlson104 <robkarlson104@gmail.com>
2026-06-17 20:54:30 -0400

    email variable

--- api.py
+++ api.py
@@ -1,10 +1,17 @@
 
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+email = os.getenv("SEC_EMAIL")
 
 sec_cik = "https://www.sec.gov/include/ticker.txt"
 
-cik_df = pd.read_table(sec_cik)
+
+
+cik_df = pd.read_table(sec_cik,)
 
 print(cik_df)
 
